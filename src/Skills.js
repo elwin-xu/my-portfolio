ff9c8c96418ef0c0b5ba864bffcc9a8b3d2ea20d
Elwin Xu <elwin.xu@icloud.com>
2023-11-18 16:18:28 +1100

    Removed Material UI and added more components

---
+++ src/Skills.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Skills() {
+  return (
+    <div>
+      <h1>My Skills</h1>
+      <p>Here's a list of my skills...</p>
+    </div>
+  );
+}
+
+export default Skills;
